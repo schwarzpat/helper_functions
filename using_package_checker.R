601af20c9791388b2c58d2e4f39f68a1650c7423
schwarzpat <82364477+schwarzpat@users.noreply.github.com>
2023-04-21 14:20:42 +0200

    Add files via upload

---
+++ using_package_checker.R
@@ -0,0 +1,16 @@
+# function that checks if required R packages are installed 
+# source is the following post: https://stackoverflow.com/a/44660688
+
+using<-function(...) {
+  libs<-unlist(list(...))
+  req<-unlist(lapply(libs,require,character.only=TRUE))
+  need<-libs[req==FALSE]
+  if(length(need)>0){ 
+    install.packages(need, repos='https://cloud.r-project.org/', dependencies=TRUE)
+    lapply(need,library,character.only=TRUE)
+  }
+}
+
+# how to use it
+#using("AcousticNDLCodeR","rstanarm", "AcrossTic", "tidymodels")
+
