3d038a7e3a12865d9c402af919e01062d461fd94
schwarzpat <82364477+schwarzpat@users.noreply.github.com>
2023-09-06 11:36:53 +0200

    Update match_merge_ibans.py

--- match_merge_ibans.py
+++ match_merge_ibans.py
@@ -92,3 +92,48 @@ def match_and_merge_accounts_efficient(input_excel_path, output_excel_path, iban
     # Data Exportation
     df_excel.to_excel(output_excel_path, index=False)
 
+import pandas as pd
+
+def match_and_merge_accounts_efficient(input_excel_path, output_excel_path, iban_csv_path):
+    """
+    Efficiently matches accounts in an Excel file against a DataFrame created from a CSV file.
+    Only the "customer_id" column from the CSV is merged into the Excel data, in a single column. 
+    NULL values are not matched.
+    
+    Parameters:
+    input_excel_path (str): The path to the input Excel file.
+    output_excel_path (str): The path where the updated Excel file will be saved.
+    iban_csv_path (str): The path to the CSV file containing the IBAN list and the "customer_id" column.
+    
+    Returns:
+    None: The function saves the updated Excel file at the specified output path.
+    """
+    
+    # Data Importation
+    df_excel = pd.read_excel(input_excel_path)
+    df_csv = pd.read_csv(iban_csv_path).dropna(subset=['iban'])
+    
+    # Create a unique identifier for each row
+    df_excel['unique_id'] = df_excel.index
+    
+    # Reshape Excel DataFrame for efficient matching
+    df_melt = df_excel.melt(id_vars=['unique_id'], 
+                            value_vars=['ordering_account', 'beneficiary_account', 'creditor_account', 'debtor_account'],
+                            var_name='account_type', value_name='account_value').dropna(subset=['account_value'])
+    
+    # Perform Merge Operation
+    df_merged = pd.merge(df_melt, df_csv[['iban', 'customer_id']], left_on='account_value', right_on='iban', how='left')
+    
+    # Aggregate to obtain a single "customer_id" per unique_id
+    df_aggregate = df_merged.groupby('unique_id')['customer_id'].first().reset_index()
+    
+    # Merge the "customer_id" column back to original DataFrame
+    df_excel = pd.merge(df_excel, df_aggregate, on='unique_id', how='left')
+    
+    # Remove the temporary unique identifier
+    df_excel.drop(columns=['unique_id'], inplace=True)
+    
+    # Data Exportation
+    df_excel.to_excel(output_excel_path, index=False)
+
+
