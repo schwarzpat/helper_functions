d24d38f3a8b7c44225cef45339ff7cf377d7c843
schwarzpat <82364477+schwarzpat@users.noreply.github.com>
2023-10-23 11:40:08 +0200

    Update embed_old_to_new.R

--- embed_old_to_new.R
+++ embed_old_to_new.R
@@ -25,3 +25,30 @@ result <- replace_values(tibble_1, tibble_2, "old_values", "new_values", "some_c
 
 # Display the result
 print(result)
+
+#########
+
+# List of unique terms
+list_of_terms <- unique(original_tibble$term)
+
+# Create a list of tibbles based on unique terms
+list_of_tibbles <- lapply(list_of_terms, function(term) {
+  filtered_tibble <- original_tibble %>% 
+                      filter(term == !!term) %>% 
+                      select(old, new)
+  return(filtered_tibble)
+})
+
+##########
+
+# Iterate over each term and corresponding tibble to assign it to a variable
+for (i in seq_along(list_of_terms)) {
+  term <- list_of_terms[[i]]
+  tibble_data <- list_of_tibbles[[i]]
+  
+  # Create a variable name based on the term
+  variable_name <- paste0("tibble_for_term_", term)
+  
+  # Assign the tibble to a variable with the generated name
+  assign(variable_name, tibble_data, envir = .GlobalEnv)
+}
