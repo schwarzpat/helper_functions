0a0f3a121445f4b7e8f218b576974d68bcfb8288
schwarzpat <82364477+schwarzpat@users.noreply.github.com>
2023-10-23 21:01:37 +0200

    Create collapse_factors.R

---
+++ collapse_factors.R
@@ -0,0 +1,18 @@
+# Load the Ames Housing dataset and transform the Sale_Price column
+data(ames, package = "modeldata")
+ames$Sale_Price <- log10(ames$Sale_Price)
+
+# Create a recipe for preprocessing the data, specifiying the columns to be collapsed by CART
+collapsed_data <-
+  recipe(Sale_Price ~ ., data = ames) %>%
+  step_collapse_cart(Sale_Type,
+                     Garage_Type,
+                     Neighborhood,
+                     Pool_QC,
+                     Functional,
+                     Exterior_1st,
+                     Exterior_2nd,
+                     MS_SubClass,
+                     outcome = vars(Sale_Price)) %>%
+  prep() %>%
+  bake(new_data = NULL)
