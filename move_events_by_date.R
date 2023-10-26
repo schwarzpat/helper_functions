826fb144f4439e520de74b9d6e771adb71edaef8
schwarzpat <82364477+schwarzpat@users.noreply.github.com>
2023-10-26 14:50:27 +0200

    Update move_events_by_date.R

--- move_events_by_date.R
+++ move_events_by_date.R
@@ -3,38 +3,43 @@ library(testthat)
 library(dplyr)
 library(lubridate)
 
-
-
-
-# Identify weekends
-df <- df %>%
-  mutate(weekend = ifelse(wday(date) %in% c(1, 7), "Yes", "No"))
-
 # Function to adjust payments
 adjust_payments <- function(df) {
+  df <- df %>%
+    mutate(weekend = ifelse(wday(date) %in% c(1, 7), "Yes", "No"))
+
   for (i in seq_len(nrow(df))) {
-    if (df$holiday[i] == "Yes") {
+    if (df$holiday[i] == "Yes" || df$weekend[i] == "Yes") {
       j <- i - 1
       while (j >= 1 && (df$holiday[j] == "Yes" || df$weekend[j] == "Yes")) {
         j <- j - 1
       }
       
       if (j >= 1) {
-        df$pay_day[j] <- df$pay_day[j] | df$pay_day[i]
-        df$pension[j] <- df$pension[j] | df$pension[i]
-        
+        if (df$pay_day[i] == 1) {
+          df$pay_day[j] <- df$pay_day[j] + df$pay_day[i]
+          df$pay_day[i] <- 0
+        }
+        if (df$pension[i] == 1) {
+          df$pension[j] <- df$pension[j] + df$pension[i]
+          df$pension[i] <- 0
+        }
+      } else {
         df$pay_day[i] <- 0
         df$pension[i] <- 0
       }
     }
   }
+
   return(df)
 }
+
+
 
 # Basic Test
 test_that("Basic test", {
   df <- tibble(
-    date = as.Date(c("2022-01-01", "2022-01-02")),
+    date = as.Date(c("2022-03-01", "2022-03-02")),
     holiday = c("No", "Yes"),
     pay_day = c(0, 1),
     pension = c(0, 0)
@@ -46,7 +51,7 @@ test_that("Basic test", {
 # Consecutive Holidays Test
 test_that("Consecutive holidays test", {
   df <- tibble(
-    date = as.Date(c("2022-01-01", "2022-01-02", "2022-01-03")),
+    date = as.Date(c("2022-01-11", "2022-01-12", "2022-01-13")),
     holiday = c("No", "Yes", "Yes"),
     pay_day = c(0, 0, 1),
     pension = c(0, 0, 0)
@@ -70,7 +75,7 @@ test_that("Weekend and holiday test", {
 # No Movement Test
 test_that("No movement test", {
   df <- tibble(
-    date = as.Date(c("2022-01-01", "2022-01-02")),
+    date = as.Date(c("2022-01-03", "2022-01-04")),
     holiday = c("No", "No"),
     pay_day = c(0, 1),
     pension = c(0, 0)
@@ -93,7 +98,7 @@ test_that("Edge case test", {
 
 test_that("Basic test for pension", {
   df <- tibble(
-    date = as.Date(c("2022-01-01", "2022-01-02")),
+    date = as.Date(c("2022-01-03", "2022-01-04")),
     holiday = c("No", "Yes"),
     pay_day = c(0, 0),
     pension = c(0, 1)
@@ -105,7 +110,7 @@ test_that("Basic test for pension", {
 # Consecutive Holidays Test for Pension
 test_that("Consecutive holidays test for pension", {
   df <- tibble(
-    date = as.Date(c("2022-01-01", "2022-01-02", "2022-01-03")),
+    date = as.Date(c("2022-01-11", "2022-01-12", "2022-01-13")),
     holiday = c("No", "Yes", "Yes"),
     pay_day = c(0, 0, 0),
     pension = c(0, 0, 1)
@@ -129,7 +134,7 @@ test_that("Weekend and holiday test for pension", {
 # No Movement Test for Pension
 test_that("No movement test for pension", {
   df <- tibble(
-    date = as.Date(c("2022-01-01", "2022-01-02")),
+    date = as.Date(c("2022-01-03", "2022-01-04")),
     holiday = c("No", "No"),
     pay_day = c(0, 0),
     pension = c(0, 1)
@@ -149,7 +154,3 @@ test_that("Edge case test for pension", {
   df <- adjust_payments(df)
   expect_equal(df$pension, c(0))
 })
-
-
-
-
