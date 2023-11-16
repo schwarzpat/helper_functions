dc2fbbc3691fcb30380b3bf9fcfed86854a2639e
schwarzpat <82364477+schwarzpat@users.noreply.github.com>
2023-11-16 12:06:26 +0100

    Update move_payday.R

--- move_payday.R
+++ move_payday.R
@@ -25,3 +25,31 @@ movePayday <- function(df, holidayColumn, paydayColumn) {
   return(df)
 }
 
+########
+
+movePaydayForward <- function(df, holidayColumn, paydayColumn) {
+  # Ensure the Date column is of Date type
+  df$Date <- as.Date(df$Date)
+  
+  # Iterate through each row
+  for (i in 1:nrow(df)) {
+    # Check if the day is a payday and either a holiday or a weekend
+    if (df[i, paydayColumn] == 1 && (df[i, holidayColumn] != "None" || df[i, "Weekday"] == 1)) {
+      # Move the payday forward
+      j <- i
+      while (df[j, holidayColumn] != "None" || df[j, "Weekday"] == 1) {
+        j <- j - 1
+        # Optional: Check for bounds of the dataframe
+        if (j < 1) {
+          break
+        }
+      }
+      if (j >= 1) {
+        # Update the payday
+        df[i, paydayColumn] <- 0
+        df[j, paydayColumn] <- 1
+      }
+    }
+  }
+  return(df)
+}
