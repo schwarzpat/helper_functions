815856ca211bdc65449a7352dda77a0b9c051cff
schwarzpat <82364477+schwarzpat@users.noreply.github.com>
2024-09-27 12:14:06 +0200

    Create remove_empty_columns.R

---
+++ remove_empty_columns.R
@@ -0,0 +1,19 @@
+install.packages("readxl")
+install.packages("writexl")
+
+library(readxl)
+library(writexl)
+
+folder_path <- "path_to_your_folder"
+
+file_list <- list.files(path = folder_path, pattern = "\\.xlsx$", full.names = TRUE)
+
+clean_excel_file <- function(file_path) {
+  data <- read_excel(file_path)
+  clean_data <- data[, colSums(is.na(data)) != nrow(data)]
+  file_name <- basename(file_path)
+  clean_file_name <- sub("\\.xlsx$", "_clean.xlsx", file_name)
+  write_xlsx(clean_data, file.path(folder_path, clean_file_name))
+}
+
+sapply(file_list, clean_excel_file)
